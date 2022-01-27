f496431078afb5b7bc11848e502ae1d1914505e7
lhrbuxiaoxin <lihaoran228@163.com>
2022-01-28 01:07:06 +0800

    change url back

--- src/WebAPIs/WebAPI.ts
+++ src/WebAPIs/WebAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-//axios.defaults.baseURL="https://rolebasedauthorization.azurewebsites.net"
-axios.defaults.baseURL="http://101.35.22.231"
+axios.defaults.baseURL="https://rolebasedauthorization.azurewebsites.net"
+//axios.defaults.baseURL="http://101.35.22.231"
 
 const WebAPI = axios
 export default WebAPI
